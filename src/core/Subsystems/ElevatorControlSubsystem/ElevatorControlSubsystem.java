87e7d3f06c61509bcc9a58c0103d1e70b20c8488
rjbansal <rajatb9703@gmail.com>
2019-01-20 15:13:34 -0500

    fix log

--- src/core/Subsystems/ElevatorControlSubsystem/ElevatorControlSubsystem.java
+++ src/core/Subsystems/ElevatorControlSubsystem/ElevatorControlSubsystem.java
@@ -1,4 +1,35 @@
+//****************************************************************************
+//
+// Filename: ElevatorControlSubsystem.java
+//
+// Description: Elevator Control Subsystem Class
+//
+//***************************************************************************
+
 package core.Subsystems.ElevatorControlSubsystem;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import core.LoggingManager;
+
 public class ElevatorControlSubsystem {
+
+	private static Logger logger = LogManager.getLogger(ElevatorControlSubsystem.class);
+	
+	public static void main(String[] args) {
+		
+		logger.info(LoggingManager.BANNER + "Elevator Subsystem\n");
+		
+		try {
+			
+		} catch (Exception e) {
+			logger.error("", e);
+            System.exit(-1);
+		}
+		
+		System.exit(0);
+		
+	}
+
 }
