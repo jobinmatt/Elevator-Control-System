69ddea51a3b4fc6482b211e0dcdb54bf61e30007
Shounak Amladi <shounak.p.amladi@gmail.com>
2019-01-20 15:20:11 -0500

    created basis for elevator subsystem

--- src/core/Subsystems/ElevatorSubsystem/ElevatorSubsystem.java
+++ src/core/Subsystems/ElevatorSubsystem/ElevatorSubsystem.java
@@ -1,4 +1,29 @@
 package core.Subsystems.ElevatorSubsystem;
-
+/**
+ * 
+ * This creates an elevator car thread pool, and powers them on. Will also handle elevator cars 
+ * @author Shounak Amladi
+ * */
 public class ElevatorSubsystem {
+	
+	
+	private int numFloors, numElev;
+	private ElevatorCar[] carPool; //lol
+	public ElevatorSubsystem(int numElev, int numFloors) {
+		this.numElev = numElev;
+		this.numFloors = numFloors;
+		this.carPool = new ElevatorCar[numElev];
+		for (int i=0; i<this.numElev;i++) {
+			this.carPool[i]= new ElevatorCar(numFloors);
+		}
+	}
+	
+	public void activateElevators() {
+		for (ElevatorCar cars : this.carPool) {
+			cars.start();
+		}
+	}
+	
+	//other elevator stuff goes here
+	
 }
--- src/core/main/ElevatorSubsystemMain.java
+++ src/core/main/ElevatorSubsystemMain.java
@@ -10,6 +10,8 @@ package core.main;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import core.Subsystems.ElevatorSubsystem.ElevatorSubsystem;
+
 public class ElevatorSubsystemMain {
 
 	private static Logger logger = LogManager.getLogger(ElevatorSubsystemMain.class);
@@ -19,9 +21,14 @@ public class ElevatorSubsystemMain {
 		logger.info(LoggingManager.BANNER + "Elevator Subsystem\n");
 		
 		try {
+			ConfigurationParser configurationParser = ConfigurationParser.getInstance();
 			
+			int numElev = configurationParser.getInt(ConfigurationParser.NUMBER_OF_ELEVATORS);
+			int numFloors = configurationParser.getInt(ConfigurationParser.NUMBER_OF_FLOORS);
+			ElevatorSubsystem eSystem = new ElevatorSubsystem(numElev, numFloors);
+			eSystem.activateElevators();
 		} catch (Exception e) {
-			logger.error("", e);
+			logger.error("Unknown error occured", e);
             System.exit(-1);
 		}
 		
